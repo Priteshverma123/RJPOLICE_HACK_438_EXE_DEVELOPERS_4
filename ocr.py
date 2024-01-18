e09836765002d6197772a80cb84c6819012dbc1a
VardhanSurve <harshvardhan.surve@gmail.com>
2024-01-18 11:02:27 +0530

    commit

--- ocr.py
+++ ocr.py
@@ -2,4 +2,4 @@ import easyocr
 def ocr(img):
     reader= easyocr.Reader(['en','hi'],gpu=True)
     result=reader.readtext(img, low_text=0.4,detail=0,paragraph=True,contrast_ths=0.5)
-    print(result)
+    return result
